815642fd52558edf8cda8c421bee69c1e3b1c16b
Micah Lau <micahelau@gmail.com>
2021-12-05 13:52:10 -0800

    Export User model through main

--- src/models/main.js
+++ src/models/main.js
@@ -1,6 +1,7 @@
 const Swimmer = require("./swimmers");
 const Coach = require("./coaches");
+const User = require("./users")
 const Event = require("./events");
 
 
-module.exports = {Swimmer, Coach, Event};
+module.exports = {Swimmer, Coach, User, Event};
