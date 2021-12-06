262c000ad1cbacb2cde9ad375db5647c6728cdca
Micah Lau <micahelau@gmail.com>
2021-12-05 17:45:57 -0800

    Update authorization middleware functionality

--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -1,6 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+
 const authorize = (req, res, next) => {
-    console.log("Authorized");
-    next();
-}
+    const token = req.body.token || req.query.token || req.headers["x-access-token"];
+    if (!token) {
+        return res.status(403).json({status: "fail", data: "An authtoken is required for authentication."});
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+        req.user = decoded;
+    } catch (err) {
+        return res.status(401).json({status: "fail", data: err.message});
+    }
+    return next();
+};
 
 module.exports = authorize;
